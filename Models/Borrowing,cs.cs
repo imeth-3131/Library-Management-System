0d090bd5cd92c31026a8b0c6c95b85296df1ead8
Imeth <Imethwickramage@gmail.com>
2026-08-14 14:33:25 +0530

    Added Borrowing models

---
+++ Models/Borrowing,cs.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library_Management_System.Models
+{
+    public class Borrowing
+    {
+        public int BorrowID { get; set; }
+
+        [Required]
+        public int BookID { get; set; }
+
+        [Required]
+        public int MemberID { get; set; }
+
+        [Required]
+        public DateTime BorrowDate { get; set; }
+
+        [Required]
+        public DateTime DueDate { get; set; }
+
+        public DateTime? ReturnDate { get; set; }
+
+        // Relationships
+        public Book Book { get; set; }
+
+        public Member Member { get; set; }
+    }
+}
