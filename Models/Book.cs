7c2344f18a9fc1da773686a2b04417711fc84abc
Imeth <Imethwickramage@gmail.com>
2026-08-14 14:23:06 +0530

    Added Book model

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,6 +1,26 @@
-﻿namespace Library_Management_System.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library_Management_System.Models
 {
     public class Book
     {
+        public int BookID { get; set; }
+
+        [Required]
+        public string ISBN { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        public string Category { get; set; }
+
+        public int PublishedYear { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int AvailableQuantity { get; set; }
     }
 }
