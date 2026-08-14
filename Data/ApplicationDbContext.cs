ebf2dc119049b3458344823d6f72e80a137038bd
Imeth <Imethwickramage@gmail.com>
2026-08-14 15:03:37 +0530

    created data folder

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Library_Management_System.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library_Management_System.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+        public DbSet<Member> Members { get; set; }
+
+        public DbSet<Borrowing> Borrowings { get; set; }
+
+        public DbSet<user> Users { get; set; }
+    }
+}
