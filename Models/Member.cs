c1eff9a6b3bb5a8d8425436c511aa583fca8fb2f
Imeth <Imethwickramage@gmail.com>
2026-08-14 14:26:16 +0530

    added member models

---
+++ Models/Member.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library_Management_System.Models
+{
+    public class Member
+    {
+        public int MemberID { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Address { get; set; }
+
+        public DateTime RegistrationDate { get; set; }
+    }
+}
