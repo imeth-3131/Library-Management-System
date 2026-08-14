2b262727cfe071091cfa5b9a297de0c99ca10c75
Imeth <Imethwickramage@gmail.com>
2026-08-14 14:37:45 +0530

    created User model

---
+++ Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library_Management_System.Models
+{
+    public class user
+    {
+        public int UserID { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
